440f39e2f9d402e2656c60e8a72733fc75cc40c0
regularwave <regularwave@gmail.com>
2025-08-06 20:29:35 -0400

    cleanup

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 function processinput() {
-
     const rawInputText = document.getElementById('inputtext').value;
     const outputTextBox = document.getElementById('outputtext');
     const inputList = rawInputText.split('\n').filter(item => item.trim() !== '');
-
     const resultList = getUniqueValues(inputList);
-
     if (resultList.length > 0) {
         outputTextBox.textContent = resultList.join('\n');
     } else {
         outputTextBox.textContent = "no unique values";
     }
-
-
-
 }
 
 function getUniqueValues(inputArray) {
